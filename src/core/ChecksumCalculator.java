f02cb6d5a71c1c58f09c33334381032e6a1ab6ca
AdrianoSiqueira <as.adriano.siqueira@gmail.com>
2018-11-18 19:50:42 -0200

    Fix parse bytes to string.

--- src/core/ChecksumCalculator.java
+++ src/core/ChecksumCalculator.java
@@ -3,7 +3,6 @@ package core;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.*;
 
@@ -30,7 +29,21 @@ public class ChecksumCalculator {
                     while ((readBytes = stream.read(buffer)) > -1)
                         digest.update(buffer, 0, readBytes);
 
-                    calculatedHashes.put(i, new BigInteger(1, digest.digest()).toString(16));
+                    /*
+                     * The following code works well
+                     * final String hash = new BigInteger(1, digest.digest()).toString(16);
+                     *
+                     * But if the calculated hash starts with '0', this '0' will
+                     * be ignored at conversion time.
+                     *
+                     * The following loop fix this limitation.
+                     */
+
+                    final StringBuilder hash = new StringBuilder();
+                    for (byte b : digest.digest())
+                        hash.append(String.format("%02x", b));
+
+                    calculatedHashes.put(i, hash.toString());
                 } catch (Exception e) {
                     System.err.println(e.toString());
                 }
